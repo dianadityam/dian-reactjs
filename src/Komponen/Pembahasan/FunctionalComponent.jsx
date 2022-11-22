6e7bfe0261fe96783676208c6ae2fbafb019dce8
Dian Aditya <dian.aditya53@gmail.com>
2022-11-22 21:33:19 +0700

    create functional component

---
+++ src/Komponen/Pembahasan/FunctionalComponent.jsx
@@ -0,0 +1,9 @@
+const FunctionalComponent = () => {
+    return (
+        <div>
+            <h2>This heading created with functional component</h2>;
+        </div>
+    );
+
+}
+export default FunctionalComponent;
