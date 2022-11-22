19a620377accb10d007540f577ea58a620477695
Dian Aditya <dian.aditya53@gmail.com>
2022-11-22 21:20:49 +0700

    create class component

---
+++ src/Komponen/Pembahasan/ClassComponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <h1>Learn Basic ReactJS Component</h1>
+                <h2>This heading created with class component</h2>
+            </div>
+        )
+
+    }
+}
+
+export default ClassComponent;
